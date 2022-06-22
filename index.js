29dee1655807b4b2c9117ce83a47e5906be1a6ae
Shamim Reza <wpshamim@gmail.com>
2022-06-22 15:12:02 +0600

    generate jwt and verify token for user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,24 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+const verifyJWT = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json("unauthorized access");
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEYS, async (err, decoded) => {
+        if (err) {
+            return res.status(403).json("Forbidden access");
+        } else {
+            console.log("decoded", decoded);
+            req.decoded = decoded;
+            console.log("inside jwtVerify");
+        }
+        await next();
+    });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yanaeka.mongodb.net`;
 
@@ -18,6 +37,19 @@ async function run() {
         await client.connect();
 
         const serviceCollections = client.db("geniusCar").collection("services");
+        const orderCollections = client.db("geniusCar").collection("orders");
+
+        // AUTH API
+
+        app.post("/getToken", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.SECRET_KEYS, {
+                expiresIn: "1d",
+            });
+            res.send({ accessToken });
+        });
+
+        // SERVICES API
 
         app.get("/services", async (req, res) => {
             const query = {};
@@ -45,15 +77,44 @@ async function run() {
             const result = await serviceCollections.deleteOne(query);
             res.send(result);
         });
+
+        app.post("/order", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollections.insertOne(order);
+            res.send(result);
+        });
+
+        app.get("/order-track/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { orderId: id };
+            const order = await orderCollections.findOne(query);
+            res.send(order);
+        });
+
+        app.get("/orders", verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded?.email;
+            const email = req.query.email;
+            if (email) {
+                if (decodedEmail === email) {
+                    const query = { email: email };
+                    const cursor = orderCollections.find(query);
+                    const orders = await cursor.toArray();
+                    res.send(orders);
+                } else {
+                    res.status(403).send({ message: "Forbidden access" });
+                }
+            } else {
+                const cursor = orderCollections.find({});
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+        });
     } finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
 
-// user: genius-car-service
-// password: F2PWvqoBuuFQzI2x
-
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Server is running..."));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
